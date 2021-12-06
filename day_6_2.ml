e338a580b1f9c39f05a20f2c08fa8bc15527a86c
professeurb <olibrunet@free.fr>
2021-12-06 08:03:26 +0100

    Day 6, on étudie les poissons-lanterne

---
+++ day_6_2.ml
@@ -0,0 +1,37 @@
+(* Day 6 *)
+(* On étudie les poissons-lanterne. *)
+
+let id x = x
+
+(* Bien sûr, on ne représente pas la colonie de poissons
+   par la liste des poissons avec pour chacun sa date de
+   reproduction, mais par un tableau donnant pour
+   chaque jour de reproduction le nombre de poissons. *)
+
+let _ =
+  let input = Scanf.Scanning.open_in "input_6.txt" in
+  try
+    let fishs = Array.make 9 0 in
+    let d = Scanf.bscanf input "%d" id in
+    fishs.(d) <- fishs.(d) + 1;
+    (try
+       while true do
+         let d = Scanf.bscanf input ",%d" id in
+         fishs.(d) <- fishs.(d) + 1
+       done
+     with Stdlib.Scanf.Scan_failure _ -> ());
+    Scanf.Scanning.close_in input;
+    for i = 1 to 256 do
+      let d0 = fishs.(0) in
+      for j = 1 to 8 do
+        fishs.(j - 1) <- fishs.(j)
+      done;
+      fishs.(8) <- d0;
+      fishs.(6) <- fishs.(6) + d0
+    done;
+    Printf.printf "%d\n"
+      (Array.fold_left (fun a b -> a + b) 0 fishs)
+  with e ->
+    Scanf.Scanning.close_in input;
+    raise e
+;;
